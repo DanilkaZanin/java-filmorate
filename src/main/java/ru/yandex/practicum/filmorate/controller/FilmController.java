3c7e2fc6b1db273a803bb3a0bd438eff59599b41
danila <y-zelenov@mail.ru>
2024-06-16 14:15:30 +0300

    Добавить валидацию

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,19 +1,20 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
-    private final HashMap<Integer, Film> films = new HashMap<>();
+    private final HashMap<Long, Film> films = new HashMap<>();
+    private static final LocalDate EARLIEST_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+
     /*
     название не может быть пустым;
     максимальная длина описания — 200 символов;
@@ -22,30 +23,42 @@ public class FilmController {
 
     @PostMapping
     public Film createFilm(@RequestBody Film film) {
-        if(checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())) {
+        film.setId(getNextId());
+        if (checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())
+                && checkDuration(film.getDuration()) && checkFilmName(film.getName())) {
             films.put(film.getId(), film);
+
+            log.info("Film crated {}", film);
             return film;
         } else {
+            log.info("Film validation exception");
             throw new ValidationException("Проверьте корректность полей!");
         }
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
-        if(films.containsKey(film.getId())) {
-            if(checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())) {
+        log.info("Началось добавление фильма!");
+        if (films.containsKey(film.getId())) {
+            if (checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())
+                    && checkDuration(film.getDuration()) && checkFilmName(film.getName())) {
                 films.put(film.getId(), film);
+
+                log.info("Film updated {}", film);
                 return film;
             } else {
+                log.info("Film validation exception");
                 throw new ValidationException("Проверьте корректность полей!");
             }
         } else {
+            log.info("Не удалось найти фильм");
             throw new NoSuchElementException("Не удалось найти фильм!");
         }
     }
 
     @GetMapping
     public List<Film> getAllFilms() {
+        log.info("Films list was got");
         return new ArrayList<>(films.values());
     }
 
@@ -53,11 +66,24 @@ public class FilmController {
         return description.length() <= 200;
     }
 
-    private boolean checkReleaseDate(LocalDateTime releaseDate) {
-        return releaseDate.isAfter(LocalDateTime.of(1895,12,28,0,0));
+    private boolean checkReleaseDate(LocalDate releaseDate) {
+        return releaseDate.isAfter(EARLIEST_RELEASE_DATE);
     }
 
-    /*private boolean checkDuration(Duration duration) {
+    private boolean checkDuration(Integer duration) {
+        return duration > 0;
+    }
 
-    }*/
+    private boolean checkFilmName(String filmName) {
+        return !Objects.equals(filmName, "");
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,8 +14,9 @@ import java.util.NoSuchElementException;
 // пока не понятно как приложение должно реагировать на пустые значения!
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
-    private final HashMap<Integer, User> users = new HashMap<>();
+    private final HashMap<Long, User> users = new HashMap<>();
 
     /* электронная почта не может быть пустой и должна содержать символ @;
         логин не может быть пустым и содержать пробелы;
@@ -24,43 +26,62 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody final User user) {
-        if(checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
+        user.setId(getNextId());
+        if (checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
+            if (user.getName() == null) {
+                user.setName(user.getLogin());
+            }
+
             users.put(user.getId(), user);
+
+            log.info("User created: {}", user);
             return user;
         } else {
+            log.info("User creation failed: {}", user);
             throw new ValidationException("Проверьте корректность полей!");
         }
     }
 
     @PutMapping
     public User updateUser(@RequestBody final User user) {
-        if(users.containsKey(user.getId())) {
-            if(checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
+        if (users.containsKey(user.getId())) {
+            if (checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
+                if (user.getName() == null) {
+                    user.setName(user.getLogin());
+                }
+
                 users.put(user.getId(), user);
+
+                log.info("User updated: {}", user.getId());
                 return user;
             } else {
+                log.info("User update failed with ValidationException: {}", user);
                 throw new ValidationException("Проверьте корректность полей!");
             }
         } else {
+            log.info("User update failed with noSuchElementException: {}", user);
             throw new NoSuchElementException("Такого пользователя нет!");
         }
     }
 
     @GetMapping
     public List<User> getUsers() {
+        log.info("Users list was got");
         return new ArrayList<>(users.values());
     }
 
     /**
      * Проверка на наличие символа @ в мэйле
+     *
      * @return true когда символ @ содержится, false когда символа @ нет
-     * */
+     */
     private boolean checkEmail(String email) {
         return email.contains("@");
     }
 
     /**
      * Проверка на наличие пробела в логине
+     *
      * @return true если пробелов нет, false если он есть
      */
     private boolean checkLogin(String login) {
@@ -69,9 +90,19 @@ public class UserController {
 
     /**
      * Проверка на корректность даты рождения
+     *
      * @return true если дата рождения была в прошлом, false если дата в будущем
-     * */
-    private boolean checkBirthday(LocalDateTime birthday) {
-        return birthday.isBefore(LocalDateTime.now());
+     */
+    private boolean checkBirthday(LocalDate birthday) {
+        return birthday.isBefore(LocalDate.now());
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
     }
 }
