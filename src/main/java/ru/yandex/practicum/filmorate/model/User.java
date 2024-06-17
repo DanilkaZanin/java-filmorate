2e159cf7e12c7bc5920a973316fe878c662e9689
danila <y-zelenov@mail.ru>
2024-06-17 18:54:44 +0300

    Добавить аннотации

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+
 
 import java.time.LocalDate;
 
@@ -11,11 +17,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class User {
     private long id;
-    @NonNull
+
+    @NotNull
+    @NotBlank
     private String login;
-    @NonNull
+
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+
     private String name;
+
+    @Past
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
