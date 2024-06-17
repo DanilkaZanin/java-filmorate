a51b8a44107fc02d8e60408a67e7adf64802b6fe
danila <y-zelenov@mail.ru>
2024-06-17 18:53:08 +0300

    Убрать лишние методы

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,11 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.*;
 
 @RestController
@@ -13,37 +12,25 @@ import java.util.*;
 @Slf4j
 public class FilmController {
     private final HashMap<Long, Film> films = new HashMap<>();
-    private static final LocalDate EARLIEST_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    //private static final LocalDate EARLIEST_RELEASE_DATE = LocalDate.of(1895, 12, 28);
 
     @PostMapping
-    public Film createFilm(@RequestBody Film film) {
-        if (checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())
-                && checkDuration(film.getDuration()) && checkFilmName(film.getName())) {
-            film.setId(getNextId());
-            films.put(film.getId(), film);
+    public Film createFilm(@Valid @RequestBody Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
 
-            log.info("Film creation failed: {}", film);
-            return film;
-        } else {
-            log.info("Film validation exception");
-            throw new ValidationException("Проверьте корректность полей!");
-        }
+        log.info("Film created: {}", film);
+        return film;
     }
 
     @PutMapping
-    public Film updateFilm(@RequestBody Film film) {
-        log.info("Началось добавление фильма!");
+    public Film updateFilm(@Valid @RequestBody Film film) {
         if (films.containsKey(film.getId())) {
-            if (checkDescriptionLength(film.getDescription()) && checkReleaseDate(film.getReleaseDate())
-                    && checkDuration(film.getDuration()) && checkFilmName(film.getName())) {
-                films.put(film.getId(), film);
+            films.put(film.getId(), film);
 
-                log.info("Film updating failed: {}", film);
-                return film;
-            } else {
-                log.info("Film validation exception");
-                throw new ValidationException("Проверьте корректность полей!");
-            }
+            log.info("Film updated: {}", film);
+            return film;
+
         } else {
             log.info("Film update failed with noSuchElementException: {}", film);
             throw new NoSuchElementException("Не удалось найти фильм!");
@@ -54,22 +41,6 @@ public class FilmController {
     public List<Film> getAllFilms() {
         log.info("Films list was got");
         return new ArrayList<>(films.values());
-    }
-
-    private boolean checkDescriptionLength(String description) {
-        return description.length() <= 200;
-    }
-
-    private boolean checkReleaseDate(LocalDate releaseDate) {
-        return releaseDate.isAfter(EARLIEST_RELEASE_DATE);
-    }
-
-    private boolean checkDuration(Integer duration) {
-        return duration > 0;
-    }
-
-    private boolean checkFilmName(String filmName) {
-        return !Objects.equals(filmName, "");
     }
 
     private long getNextId() {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,11 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,38 +17,28 @@ public class UserController {
     private final HashMap<Long, User> users = new HashMap<>();
 
     @PostMapping
-    public User createUser(@RequestBody final User user) {
-        if (checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
-            if (user.getName() == null || user.getName().trim().isEmpty()) {
-                user.setName(user.getLogin());
-            }
-            user.setId(getNextId());
-            users.put(user.getId(), user);
-
-            log.info("User created: {}", user);
-            return user;
-        } else {
-            log.info("User creation failed: {}", user);
-            throw new ValidationException("Проверьте корректность полей!");
+    public User createUser(@Valid @RequestBody final User user) {
+        if (user.getName() == null || user.getName().trim().isEmpty()) {
+            user.setName(user.getLogin());
         }
+
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+
+        log.info("User created: {}", user);
+        return user;
     }
 
     @PutMapping
-    public User updateUser(@RequestBody final User user) {
+    public User updateUser(@Valid @RequestBody final User user) {
         if (users.containsKey(user.getId())) {
-            if (checkEmail(user.getEmail()) && checkLogin(user.getLogin()) && checkBirthday(user.getBirthday())) {
-                if (user.getName() == null || user.getName().trim().isEmpty()) {
-                    user.setName(user.getLogin());
-                }
-
-                users.put(user.getId(), user);
-
-                log.info("User updated: {}", user.getId());
-                return user;
-            } else {
-                log.info("User update failed with ValidationException: {}", user);
-                throw new ValidationException("Проверьте корректность полей!");
+            if (user.getName() == null || user.getName().trim().isEmpty()) {
+                user.setName(user.getLogin());
             }
+
+            users.put(user.getId(), user);
+            log.info("User updated: {}", user.getId());
+            return user;
         } else {
             log.info("User update failed with noSuchElementException: {}", user);
             throw new NoSuchElementException("Такого пользователя нет!");
@@ -60,33 +49,6 @@ public class UserController {
     public List<User> getUsers() {
         log.info("Users list was got");
         return new ArrayList<>(users.values());
-    }
-
-    /**
-     * Проверка на наличие символа @ в мэйле
-     *
-     * @return true когда символ @ содержится, false когда символа @ нет
-     */
-    private boolean checkEmail(String email) {
-        return email.contains("@");
-    }
-
-    /**
-     * Проверка на наличие пробела в логине
-     *
-     * @return true если пробелов нет, false если он есть
-     */
-    private boolean checkLogin(String login) {
-        return !login.isEmpty() && !login.contains(" ");
-    }
-
-    /**
-     * Проверка на корректность даты рождения
-     *
-     * @return true если дата рождения была в прошлом, false если дата в будущем
-     */
-    private boolean checkBirthday(LocalDate birthday) {
-        return birthday.isBefore(LocalDate.now());
     }
 
     private long getNextId() {
