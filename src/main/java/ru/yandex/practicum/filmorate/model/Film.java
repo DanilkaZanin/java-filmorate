b45ed898861c555ff057ab4ba8a2b111ab456cbc
danila <y-zelenov@mail.ru>
2024-06-17 18:54:21 +0300

    Добавить аннотации, реализовать аннотацию для проверки даты релиза

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,9 +2,14 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import ru.yandex.practicum.filmorate.validation.annotation.ReleaseDate;
 
 import java.time.LocalDate;
 
@@ -15,10 +20,20 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Film {
     private long id;
-    @NonNull
+
+    @NotNull
+    @NotBlank
     private String name;
+
+    @Size(min = 1, max = 200)
     private String description;
+
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @ReleaseDate(earliestDate = "1895-12-28")
+    @NotNull
     private LocalDate releaseDate;
+
+    @Min(1)
     private int duration;
 }
