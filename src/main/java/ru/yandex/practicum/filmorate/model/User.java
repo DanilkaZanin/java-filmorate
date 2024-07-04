2c6d3b3684fc5757e9bd497c8e5226c331b82981
danila <y-zelenov@mail.ru>
2024-07-04 12:17:44 +0300

    Добавить список друзей и методы по добавлению/удалению друзей

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,18 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.yandex.practicum.filmorate.validation.annotation.NoSpaceInLogin;
 
-
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -34,4 +33,14 @@ public class User {
     @Past
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
+    private Set<Long> friends = new HashSet<>();
+
+    public void putFriend(long friendId) {
+        friends.add(friendId);
+    }
+
+    public void deleteFriend(long friendId) {
+        friends.remove(friendId);
+    }
 }
