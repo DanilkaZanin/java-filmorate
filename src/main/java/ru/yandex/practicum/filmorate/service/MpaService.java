e392c8b149fc11b418f2faee6319b6a883a0dfb8
danila <y-zelenov@mail.ru>
2024-07-28 19:20:54 +0300

    Убрать неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.repository.MpaRepository;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
