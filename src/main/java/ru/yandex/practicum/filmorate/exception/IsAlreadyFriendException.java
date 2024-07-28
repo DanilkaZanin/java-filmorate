b0df6e573ecb28e693353e00ffd386ca4cb383eb
danila <y-zelenov@mail.ru>
2024-07-28 16:07:03 +0300

    Удалить ненужное

--- src/main/java/ru/yandex/practicum/filmorate/exception/IsAlreadyFriendException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class IsAlreadyFriendException extends RuntimeException {
-    public IsAlreadyFriendException(final String message) {
-        super(message);
-    }
-}
