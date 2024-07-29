df94fd7cb10dd7f2bb9c3feb418f312edd865158
danila <y-zelenov@mail.ru>
2024-07-29 13:02:57 +0300

    Удалить exception

--- src/main/java/ru/yandex/practicum/filmorate/exception/IsAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class IsAlreadyExistsException extends RuntimeException {
-    public IsAlreadyExistsException(final String message) {
-        super(message);
-    }
-}
